[correction] Exported cylinder corrected. MonteCarlo rotation corrected for class

dev/main.py
@@ -1,19 +1,26 @@
+import numpy as np
+
 from modules.polymer import Polymer
+from modules.montecarlo import MonteCarlo
 from modules.exporter import Exporter
 
 
 
 
 if __name__ == "__main__":
-    N = 500
+    N = 20
     na = Polymer(N)
-    
+    mc = MonteCarlo(na)
     """
     for i in range(0,N):
         print(1/na.c[i])
     """
     
+    
+    mc.rot_section(4, 9, np.pi/2)
+    na.get_geometry()
     #print(na.calc_writhe())
-    print(na.unknoted())
+    #print(na.unknoted())
     exporter = Exporter(na, "./")
     exporter.save_XYZ_cylinders(0)
+    #exporter.save_XYZ(0)

dev/modules/exporter.py
@@ -20,15 +20,17 @@ class Exporter():
     """
         TODO: CHECK WHAT HAPPENS IF YOU MODIFY THE POLYMER
         BETTER JUST GIVE THE OBJECT AS ARGUMENT
+        Checked! Nonetheless, further confirmations necessary
     """
     def save_XYZ(self, fileid):
         """
             Save file in XYZ for representation with OVITO
         """
-        np.savetxt(str(fileid) + "_na.dat", 
+        np.savetxt(str(fileid) + "_na.xyz", 
                     self.polymer.r, 
                     delimiter="\t",
-                    header=str(self.N) + "\n\n")
+                    header=str(self.N) + "\n",
+                    comments='')
     
     
     def save_XYZ_cylinders(self, fileid):
@@ -36,7 +38,11 @@ class Exporter():
         
             Save extended file in XYZ for representation with OVITO
             
-            TODO: Add extender description of how is organized the file
+            The file is composed by 10 columns. The 3 first are the 
+            coordinates of the center of mass of the cylinder. The next 4 
+            are a quaternion (3 coordinates for an axis  of rotation + angle). 
+            The next 3 are associated to aspherical_shape dictates the 
+            radius, asphericity (0 for a cylinder), and the length.
             
             See https://www.ovito.org/forum/topic/quaternion-representation-of-aspherical-particels/
             for a description of the quaternion approximation
@@ -52,10 +58,10 @@ class Exporter():
             
             # Quaternion for the rotation with respect to z axis
             q[0:3] = np.cross(np.array([0,0,1]), self.polymer.t[i,:])
-            q[3] = 1 + self.polymer.t[i,2]
+            q[3] = 1 + self.polymer.t[i,2] #q[3] = 1 + dot(v1,v2);
             q /= np.linalg.norm(q)
                         
-            cm = self.polymer.r[i,:] + self.polymer.ds[i]*self.polymer.t[i,:]
+            cm = self.polymer.r[i,:] + self.polymer.ds[i]*self.polymer.t[i,:]/2
             
             filexyz.writelines(str(cm[0]) + "\t" + str(cm[1]) + "\t" + str(cm[2]) +  "\t" +                  # Center of mass of the cylinder
                                str(q[0]) + "\t" + str(q[1]) + "\t" + str(q[2]) +  "\t" + str(q[3]) + "\t" +  # Quaternion for orientation

dev/modules/montecarlo.py
@@ -1,12 +1,12 @@
 import numpy as np
 from numba import jit, njit
 
-from polymer import Polymer
+from modules.polymer import Polymer
 
 
 
 
-Class MonteCarlo():
+class MonteCarlo():
     """
     Class MonteCarlo
     ------------------
@@ -16,16 +16,21 @@ Class MonteCarlo():
     Attributes:
     -----------
     
+    
     """
 
-    def __init__(self, params) -> None:
+    def __init__(self, polymer, params=1) -> None:
         """
+            params are the range of angles, the maximum extent of rotation, etc
         """        
         self.kBT = 4.1
         self.r = 1
-
         
-    def mcstep(polymer):
+        self.polymer = polymer 
+        self.polymer_test = polymer
+        
+    """
+    def mcstep():
         E_curr = polymer.E
         
         polymer_test = copy_object
@@ -49,37 +54,32 @@ Class MonteCarlo():
         
         return p
         
-    
-    
-    
-    
-    
-    
-    
-    
-    def rot_section(r, id0, idf, alpha):
     """
-        Uses Rodrigues' rotation formula to rotate the set
-        of verticies between indexes id0 and idf by the 
-        angle alpha
-    """
-        r_rot = np.copy(r)
-        k = r[idf,:] - r[id0,:]
+    
+    
+    def rot_section(self, id0, idf, alpha):
+        """
+            Uses Rodrigues' rotation formula to rotate the set
+            of verticies between indexes id0 and idf by the 
+            angle alpha
+        """
+        r_rot = np.copy(self.polymer.r)
+        k = self.polymer.r[idf,:] - self.polymer.r[id0,:]
         k /= np.linalg.norm(k)
         
         for i in range(id0+1, idf):
-            v = r[i,:] - r[id0,:]
+            v = self.polymer.r[i,:] - self.polymer.r[id0,:]
             r_rot[i,:] = v*np.cos(alpha) +  np.cross(k,v)*np.sin(alpha) + k*(1-np.cos(alpha))*np.dot(k,v)
-            r_rot[i,:] += r[id0,:]
+            r_rot[i,:] += self.polymer.r[id0,:]
+        
+        # TODO: THIS HAS TO BE CHANGED TO USE THE TEMP self.polymer_test
+        self.polymer.r = np.copy(r_rot)
+    
 
-        return r_rot
-    
-    
-    
-    
-    
-    def check_intersect(r, N, eps):
     """
+    TODO: Note that in the intersection we only need to check the data that has been rotated!
+    def check_intersect(r, N, eps):
+
         Check if there is overlap between the curves in
     
         Input: 
@@ -89,7 +89,7 @@ Class MonteCarlo():
         Output: 
                 intersect = bool, Returns true if two cylinders 
                             intersect each other
-    """
+
     
     intersect = False
     for i in range(0,N):
@@ -111,7 +111,7 @@ Class MonteCarlo():
             dr = r[idp,:] - r[jdp,:]
             
             delta2 = (np.dot(dr,ti)*np.dot(ti,tj) - np.dot(dr,tj)) /        \
-                     ( np.dot(ti,tj) - 1)
+                    ( np.dot(ti,tj) - 1)
             
             delta1 = delta2*np.dot(ti,tj) - np.dot(dr, ti)
             
@@ -126,4 +126,6 @@ Class MonteCarlo():
                 if d<eps:
                     intersect = True
                     break
-    
+                
+                
+    """

dev/modules/polymer.py
@@ -41,9 +41,9 @@ class Polymer():
         self.c  = np.zeros((N))
     
         
-        self.init_tree_foil_polymer()
+        #self.init_tree_foil_polymer()
         #self.init_figure8_knot()
-        #self.init_circular_polymer()
+        self.init_circular_polymer()
         #self.init_leminiscata()
         
         self.get_geometry()